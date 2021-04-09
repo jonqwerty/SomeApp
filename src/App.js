c66ab70794734668f76ca1ccabdd5bf0ff375181
Serhii B <s_blyshchak@ukr.net>
2021-04-09 22:58:15 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useState } from 'react'
 
-function App() {
+ const App = () => {
+
+  const appStyle = {
+    padding: '60px', 
+    fontSize: '30px', 
+    fontWeight: 'bold'
+  }
+
+  const counterStyle = {
+    padding: '20px', 
+    fontSize: '20px', 
+    fontWeight: 'bold'
+  }
+
+  const [str , setStr] = useState('')
+
+  const stringArray = str.split('\n')
+  const firstItem = stringArray[0]
+  const quontity = stringArray.filter(s => s === firstItem).length
+   
+     
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div style = {appStyle}>
+       SOME APP
+      </div>
+      <textarea 
+        placeholder='Enter some lines and enjoy the miracle...
+        
+        By the way do not forget to split lines with "Enter"' 
+        rows='15' 
+        value={str} 
+        type='text' 
+        onChange={e => setStr(e.target.value)} 
         >
-          Learn React
-        </a>
-      </header>
+        </textarea>
+      <div>
+        See, how many times the first line appears: 
+          <span style = {counterStyle}>
+            {str === '' ? 0 : quontity}
+          </span> 
+      </div>
+      <button onClick={() => setStr('')}>Clear text area</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
